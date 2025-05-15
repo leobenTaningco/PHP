272816f47f0f118a537b2666ab29da63eae25c4b
leobenTaningco <152823492+leobenTaningco@users.noreply.github.com>
2025-05-16 07:08:40 +0800

    Add files via upload

---
+++ activity7/main.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search Engine</title>
+</head>
+<body>
+ <center>
+    <form action="result.php" method="GET">
+      <table border = 1>
+            <tr>
+               <th>Search</th>
+               <td><input type ="text" name="keyword"></td>
+               <td><input type ="submit" name="submit" value="Go"></td>
+           </tr>
+    </form>
+     </table>
+
+</center>
+</body>
+</html>
